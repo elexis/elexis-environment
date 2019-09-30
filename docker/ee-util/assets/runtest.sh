3cbe9c128c8b8cf50acced7097a8ccf572940084
Marco Descher <marco@descher.at>
2019-09-30 14:26:24 +0200

    major

--- docker/ee-util/assets/runtest.sh
+++ docker/ee-util/assets/runtest.sh
@@ -3,5 +3,6 @@
 bats stage_test/tests/elexis-env.bats
 bats stage_test/tests/ldap.bats
 bats stage_test/tests/keycloak.bats
-bats stage_test/tests/bookstack.bats
 bats stage_test/tests/elexis-server.bats
+[[ $ENABLE_BOOKSTACK == true ]] && bats stage_test/tests/bookstack.bats
+[[ $ENABLE_ROCKETCHAT == true ]] && bats stage_test/tests/rocketchat.bats
