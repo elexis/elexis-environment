93c59e1128be21a5c750531abd3d8c64bd632b61
Marco Descher <marco@descher.at>
2019-09-30 13:34:37 +0200

    gitlab

--- .test/mysql/init.sql
+++ .test/mysql/init.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE ee_elexis;
-CREATE USER elexis@'172.%' IDENTIFIED BY 'elexis';
+CREATE USER elexis@'%' IDENTIFIED BY 'elexis';
 GRANT ALL PRIVILEGES ON ee_elexis.* TO 'elexis'@'%';
 CREATE DATABASE ee_bookstack;
-CREATE USER ee_bookstack@'172.%' IDENTIFIED BY 'ee_bookstack';
+CREATE USER ee_bookstack@'%' IDENTIFIED BY 'ee_bookstack';
 GRANT ALL PRIVILEGES ON ee_bookstack.* TO ee_bookstack@'%';
 CREATE DATABASE ee_keycloak CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-CREATE USER ee_keycloak@'172.%' IDENTIFIED WITH mysql_native_password BY 'ee_keycloak';
+CREATE USER ee_keycloak@'%' IDENTIFIED WITH mysql_native_password BY 'ee_keycloak';
 GRANT ALL PRIVILEGES ON ee_keycloak.* TO ee_keycloak@'%';
 FLUSH PRIVILEGES;
