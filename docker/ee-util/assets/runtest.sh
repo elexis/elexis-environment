529854f249dee5f09a1a8daa477b6c915d8dfbd0
Marco Descher <marco@descher.at>
2019-10-01 13:34:28 +0200

    Do not return 1 on runtest

--- docker/ee-util/assets/runtest.sh
+++ docker/ee-util/assets/runtest.sh
@@ -4,5 +4,9 @@ bats stage_test/tests/elexis-env.bats
 bats stage_test/tests/ldap.bats
 bats stage_test/tests/keycloak.bats
 bats stage_test/tests/elexis-server.bats
-[[ $ENABLE_BOOKSTACK == true ]] && bats stage_test/tests/bookstack.bats
-[[ $ENABLE_ROCKETCHAT == true ]] && bats stage_test/tests/rocketchat.bats
+if [[ $ENABLE_BOOKSTACK == true ]]; then
+    bats stage_test/tests/bookstack.bats
+fi
+if [[ $ENABLE_ROCKETCHAT == true ]]; then
+    bats stage_test/tests/rocketchat.bats
+fi 
