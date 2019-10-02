f95c5e5b10c0776cd823a2967bb2e39cc8ab9216
Marco Descher <marco@descher.at>
2019-10-02 15:42:23 +0200

    rocketchat.sh wait for rocketchat

--- docker/ee-util/assets/stage_ee_start_setup/rocketchat.sh
+++ docker/ee-util/assets/stage_ee_start_setup/rocketchat.sh
@@ -6,36 +6,39 @@
 RC_BASEURL="https://$EE_HOSTNAME/chat"
 T="[ROCKETCHAT] ($RC_BASEURL) "
 
-# log-in
 echo "$T Log-in as RocketChatAdmin..."
-RESPONSE=$(curl -s -k $RC_BASEURL/api/v1/login -d "user=RocketChatAdmin&password=$ADMIN_PASSWORD")
-ret=$?
-if [ $ret -ne 0 ]
-then
-    echo "Exit code ($ret) with response ($RESPONSE)"
-    exit -1
-fi
+RESPONSE=$(curl -s -k $RC_BASEURL/api/v1/login -d "user=RocketChatAdmin&password=$ADMIN_PASSWORD") 
+STATUS="$?"
+LOOP_COUNT=0
+while [ $STATUS != 0 ] 
+do 
+    echo "Waiting for rocketchat [$STATUS] ($RESPONSE) ..."
+    sleep 5
+    RESPONSE=$(curl -s -k $RC_BASEURL/api/v1/login -d "user=RocketChatAdmin&password=$ADMIN_PASSWORD") 
+    STATUS="$?"
+    (( LOOP_COUNT += 1 ))
+    if [ $LOOP_COUNT -eq 10 ]; then exit -1; fi
+done
+
 AUTH_TOKEN=$(echo $RESPONSE | jq -r .data.authToken)
 USER_ID=$(echo $RESPONSE | jq -r .data.me._id)
 
-CURL_AUTH="-H \"X-Auth-Token: $AUTH_TOKEN\" -H \"X-User-Id: $USER_ID\""
-
 echo "$T Assert channel #elexis-server..."
-curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/channels.create -d '{ "name": "elexis-server", "description": "Elexis-Server status messages" }'
+curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" -H "Content-type: application/json" $RC_BASEURL/api/v1/channels.create -d '{ "name": "elexis-server", "description": "Elexis-Server status messages" }'
 
 echo -e "\n$T Assert bot user for elexis-user..."
-curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/users.create --data-binary @rocketchat/cr_es_user.json
+curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" -H "Content-type: application/json" $RC_BASEURL/api/v1/users.create --data-binary @rocketchat/cr_es_user.json
 
 echo -e "\n$T Assert bot avatar for elexis-server ..."
-curl -s -k $CURL_AUTH -F "image=@rocketchat/elexis-server.png" -F "username=elexis-server" $RC_BASEURL/api/v1/users.setAvatar 
+curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" -F "image=@rocketchat/elexis-server.png" -F "username=elexis-server" $RC_BASEURL/api/v1/users.setAvatar 
 
 echo -e "\n$T Assert webhook integration for elexis-server..."
-EX_INTEGRATIONS=$(curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.list)
+EX_INTEGRATIONS=$(curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.list)
 EXISTING=$(echo $EX_INTEGRATIONS | jq '.integrations[] | select(.name=="elexis-server-messages")')
 if [ -z "$EXISTING" ]
 then
     echo "Creating webhook ..."
-    EXISTING=$(curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.create --data-binary @rocketchat/cr_es_inc_webhook.json)
+    EXISTING=$(curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.create --data-binary @rocketchat/cr_es_inc_webhook.json)
 fi
 echo $EXISTING
 
@@ -46,4 +49,4 @@ MYSQL_STRING="INSERT INTO CONFIG(lastupdate, param, wert) VALUES ('${LASTUPDATE}
 /usql mysql://${RDBMS_ELEXIS_USERNAME}:${RDBMS_ELEXIS_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_ELEXIS_DATABASE} -c "$MYSQL_STRING"
 
 echo "$T Log-Out as RocketChatAdmin..."
-curl -s -k $CURL_AUTH $RC_BASEURL/api/v1/logout
+curl -s -k -H "X-Auth-Token: $AUTH_TOKEN" -H "X-User-Id: $USER_ID" $RC_BASEURL/api/v1/logout
