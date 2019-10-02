81fde9508c00b2b451b732432fb2995f086077e0
Marco Descher <marco@descher.at>
2019-10-02 15:05:25 +0200

    do not assume internal hostname connectable

--- docker/ee-util/assets/stage_ee_start_setup/rocketchat.sh
+++ docker/ee-util/assets/stage_ee_start_setup/rocketchat.sh
@@ -3,14 +3,12 @@
 # Configure Rocket.Chat for Elexis Operation
 #
 
-RC_BASEURL="http://rocketchat:3000/chat"
+RC_BASEURL="https://$EE_HOSTNAME/chat"
 T="[ROCKETCHAT] ($RC_BASEURL) "
-
-ping -c 1 rocketchat
 
 # log-in
 echo "$T Log-in as RocketChatAdmin..."
-RESPONSE=$(curl -s $RC_BASEURL/api/v1/login -d "user=RocketChatAdmin&password=$ADMIN_PASSWORD")
+RESPONSE=$(curl -s -k $RC_BASEURL/api/v1/login -d "user=RocketChatAdmin&password=$ADMIN_PASSWORD")
 ret=$?
 if [ $ret -ne 0 ]
 then
@@ -23,21 +21,21 @@ USER_ID=$(echo $RESPONSE | jq -r .data.me._id)
 CURL_AUTH="-H \"X-Auth-Token: $AUTH_TOKEN\" -H \"X-User-Id: $USER_ID\""
 
 echo "$T Assert channel #elexis-server..."
-curl -s $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/channels.create -d '{ "name": "elexis-server", "description": "Elexis-Server status messages" }'
+curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/channels.create -d '{ "name": "elexis-server", "description": "Elexis-Server status messages" }'
 
 echo -e "\n$T Assert bot user for elexis-user..."
-curl -s $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/users.create --data-binary @rocketchat/cr_es_user.json
+curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/users.create --data-binary @rocketchat/cr_es_user.json
 
 echo -e "\n$T Assert bot avatar for elexis-server ..."
-curl -s $CURL_AUTH -F "image=@rocketchat/elexis-server.png" -F "username=elexis-server" $RC_BASEURL/api/v1/users.setAvatar 
+curl -s -k $CURL_AUTH -F "image=@rocketchat/elexis-server.png" -F "username=elexis-server" $RC_BASEURL/api/v1/users.setAvatar 
 
 echo -e "\n$T Assert webhook integration for elexis-server..."
-EX_INTEGRATIONS=$(curl -s $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.list)
+EX_INTEGRATIONS=$(curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.list)
 EXISTING=$(echo $EX_INTEGRATIONS | jq '.integrations[] | select(.name=="elexis-server-messages")')
 if [ -z "$EXISTING" ]
 then
     echo "Creating webhook ..."
-    EXISTING=$(curl -s $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.create --data-binary @rocketchat/cr_es_inc_webhook.json)
+    EXISTING=$(curl -s -k $CURL_AUTH -H "Content-type: application/json" $RC_BASEURL/api/v1/integrations.create --data-binary @rocketchat/cr_es_inc_webhook.json)
 fi
 echo $EXISTING
 
@@ -48,4 +46,4 @@ MYSQL_STRING="INSERT INTO CONFIG(lastupdate, param, wert) VALUES ('${LASTUPDATE}
 /usql mysql://${RDBMS_ELEXIS_USERNAME}:${RDBMS_ELEXIS_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_ELEXIS_DATABASE} -c "$MYSQL_STRING"
 
 echo "$T Log-Out as RocketChatAdmin..."
-curl -s $CURL_AUTH $RC_BASEURL/api/v1/logout
+curl -s -k $CURL_AUTH $RC_BASEURL/api/v1/logout
