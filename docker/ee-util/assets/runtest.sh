65a09e97ddc545af48370c16dfdb17b48749219e
Marco Descher <marco@descher.at>
2019-10-02 08:25:18 +0200

    gitlab-ci.yml

--- docker/ee-util/assets/runtest.sh
+++ docker/ee-util/assets/runtest.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-bats stage_test/tests/elexis-env.bats
-bats stage_test/tests/ldap.bats
-bats stage_test/tests/keycloak.bats
-bats stage_test/tests/elexis-server.bats
+bats stage_test/tests/elexis-env.bats;  (( exit_status = exit_status || $? ))
+bats stage_test/tests/ldap.bats;  (( exit_status = exit_status || $? ))
+bats stage_test/tests/keycloak.bats;  (( exit_status = exit_status || $? ))
+bats stage_test/tests/elexis-server.bats;  (( exit_status = exit_status || $? ))
 if [[ $ENABLE_BOOKSTACK == true ]]; then
-    bats stage_test/tests/bookstack.bats
+    bats stage_test/tests/bookstack.bats;  (( exit_status = exit_status || $? ))
 fi
 if [[ $ENABLE_ROCKETCHAT == true ]]; then
-    bats stage_test/tests/rocketchat.bats
+    bats stage_test/tests/rocketchat.bats;  (( exit_status = exit_status || $? ))
 fi 
+
+exit $exit_status   # 0 if they all succeeded, 1 if any failed
