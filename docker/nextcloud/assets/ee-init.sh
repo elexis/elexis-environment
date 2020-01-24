a53b9ed34b670845ace0f3f5c6a23fc02c95cf9f
Marco Descher <marco@descher.at>
2020-01-24 08:38:32 +0100

    [17826] Nextcloud show time in start script

--- docker/nextcloud/assets/ee-init.sh
+++ docker/nextcloud/assets/ee-init.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-echo "Elexis-Environment specific setup"
+echo "$(date): Elexis-Environment specific setup"
 ln -sf /var/www/html /var/www/html/cloud
-echo "Asser user_saml app is installed ..."
+echo "$(date): Assert user_saml app is installed ..."
 php /var/www/html/occ app:install user_saml
-echo "Check big int conversion ..."
+echo "$(date): Check big int conversion ..."
 php /var/www/html/occ db:convert-filecache-bigint
-echo "Check indices ..."
+echo "$(date): Check indices ..."
 php /var/www/html/occ db:add-missing-indices
+
+# TODO external storage support aktivieren?
 
 # we have to return 0, as calling script is set -e
 # which will exit if user_saml is already installed (return code)
