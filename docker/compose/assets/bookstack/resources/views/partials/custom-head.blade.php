5ebf8ba2f1e624ff7918bb5cac96307e83dd8209
Marco Descher <marco@descher.at>
2021-06-09 07:24:23 +0200

    Revert "[22719] EE Theme bookstack"
    This reverts commit 9eb57c100ff08a3a3194be84c29c4624753c0837.

--- docker/compose/assets/bookstack/resources/views/partials/custom-head.blade.php
+++
@@ -1,5 +0,0 @@
-@if(setting('app-custom-head') && \Route::currentRouteName() !== 'settings')
-    <!-- Custom user content -->
-    {!! setting('app-custom-head') !!}
-    <!-- End custom user content -->
-@endif
