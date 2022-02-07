e2a34bf38dfd1f8c36e4daa60678dfd538e238ac
Marco Descher <marco@descher.at>
2022-02-07 11:19:55 +0100

    [23530] Oauth for jaxrs-api

--- docker/ee-util/assets/stage_ee_start_setup/keycloak.sh
+++ docker/ee-util/assets/stage_ee_start_setup/keycloak.sh
@@ -192,6 +192,20 @@ echo "$T update client settings ... "
 $KCADM update clients/$ES_FHIR_OPENID_CLIENTID -r ElexisEnvironment -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$X_EE_ELEXIS_SERVER_CLIENT_SECRET -s bearerOnly=true
 
 #
+# ELEXIS-SERVER.JAXRS-API (Bearer Only)
+#
+T="$S (elexis-server.jaxrs-api)"
+ES_JAXRS_OPENID_CLIENTID=$(getClientId elexis-server.jaxrs-api | cut -d "," -f1)
+if [ -z $ES_JAXRS_OPENID_CLIENTID ]; then
+    echo -n "$T create client ... "
+    ES_JAXRS_OPENID_CLIENTID=$($KCADM create clients -r ElexisEnvironment -s clientId=elexis-server.jaxrs-api -i)
+    echo "ok $ES_JAXRS_OPENID_CLIENTID"
+fi
+
+echo "$T update client settings ... "
+$KCADM update clients/$ES_JAXRS_OPENID_CLIENTID -r ElexisEnvironment -s enabled=true -s clientAuthenticatorType=client-secret -s secret=$X_EE_ELEXIS_SERVER_CLIENT_SECRET -s bearerOnly=true
+
+#
 # ELEXIS-WEB-API-OPENID
 #
 T="$S (elexis-web-api)"
