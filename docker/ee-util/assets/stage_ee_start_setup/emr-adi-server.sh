0bbad267b61816d86566d9c611ae6f3822b5ae8d
Marco Descher <marco@descher.at>
2023-03-08 14:43:18 +0100

    [24550] adi initialization

--- docker/ee-util/assets/stage_ee_start_setup/emr-adi-server.sh
+++ docker/ee-util/assets/stage_ee_start_setup/emr-adi-server.sh
@@ -6,6 +6,24 @@ echo "$T $(date)"
 
 BASEURL="http://emr-adi-server:20141/services"
 
+#
+#
+# Wait for login
+#
+#
+LOOP_COUNT=0
+while [[ "$(curl -s -o /dev/null -k -w ''%{http_code}''  -u "admin.user:${ADMIN_PASSWORD}" $BASEURL/swagger.json)" != "200" ]]; do
+    echo "$T Waiting for emr-adi-server  ..."
+    sleep 5
+    ((LOOP_COUNT += 1))
+    if [ $LOOP_COUNT -eq 20 ]; then exit -1; fi
+done
+
+#
+#
+# Basic configuration values
+#
+#
 if [ $ENABLE_ELEXIS_SERVER == "true" ]; then
     echo "$T Setting fhirsync to elexis-server  ..."
     # delete existing sync if exists
