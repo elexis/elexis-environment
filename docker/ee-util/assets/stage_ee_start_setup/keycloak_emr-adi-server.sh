d9dc5d6492b4166804adda1d5361ec1784535d59
Marco Descher <marco@descher.at>
2023-03-09 13:58:58 +0100

    update

--- docker/ee-util/assets/stage_ee_start_setup/keycloak_emr-adi-server.sh
+++ docker/ee-util/assets/stage_ee_start_setup/keycloak_emr-adi-server.sh
@@ -10,14 +10,15 @@ if [ -z $EMR_ADI_SERVER_OPENID_CLIENT ]; then
 fi
 
 echo "$T update client settings and secret ... "
-EMR_ADI_SERVER_SECRET_UUID=$(uuidgen)
+export EMR_ADI_SERVER_SECRET_UUID=$(uuidgen)
 $KCADM update clients/$EMR_ADI_SERVER_OPENID_CLIENT -r ElexisEnvironment -s clientAuthenticatorType=client-secret -s secret=$EMR_ADI_SERVER_SECRET_UUID -s enabled=$ENABLE_EMR_ADI_SERVER  -f keycloak/emr-adi-server-openid.json
 
-EMR_ADI_SERVER_USER_NAME=emr-adi-server
-EMR_ADI_SERVER_USER_PASSWORD=$(uuidgen)
+export EMR_ADI_SERVER_USER_NAME=emr-adi-server
+export EMR_ADI_SERVER_USER_PASSWORD=$(uuidgen)
 $KCADM create users -r ElexisEnvironment -s username=$EMR_ADI_SERVER_USER_NAME -i
 echo "$T update $EMR_ADI_SERVER_USER_NAME password ..."
 $KCADM set-password -r ElexisEnvironment --username $EMR_ADI_SERVER_USER_NAME --new-password $EMR_ADI_SERVER_USER_PASSWORD
 
 # to be consumed in emr-adi-server.sh
 envsubst <emr-adi-server_synctemplate.json >emr-adi-server.json
+export -n EMR_ADI_SERVER_USER_PASSWORD
