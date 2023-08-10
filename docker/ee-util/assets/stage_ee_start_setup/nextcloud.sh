91376f7efaf8ef38f60ec5b4777efe09cf16411c
Marco Descher <marco@descher.at>
2023-08-10 14:01:23 +0200

    [25476] Remove direct SQL call

--- docker/ee-util/assets/stage_ee_start_setup/nextcloud.sh
+++ docker/ee-util/assets/stage_ee_start_setup/nextcloud.sh
@@ -27,11 +27,3 @@ done
 #echo -e "$T Assert basic configuration ..."
 #java -jar /NextcloudSetter.jar -t -l NextcloudAdmin -p $ADMIN_PASSWORD -u $NC_BASEURL -v \
 #    -s theming/name="${ORGANISATION_NAME//__/\ }" -s theming/url="$EE_HOSTNAME/cloud"
-
-#
-# OpenId configuration
-# 
-echo "$T Update oidc client secret ..."
-NC_OPENID_CLIENT_SECRET=$(cat "/tmp/NC_OPENID_CLIENT_SECRET")
-MYSQL_STRING="UPDATE oc_user_oidc_providers SET client_secret='${NC_OPENID_CLIENT_SECRET}' WHERE (identifier = 'Keycloak')"
-/usql mysql://${RDBMS_NEXTCLOUD_USERNAME}:${RDBMS_NEXTCLOUD_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_NEXTCLOUD_DATABASE} -c "$MYSQL_STRING"
