134cf0a21070910967a766db4d310a0eb63d4367
Marco Descher <marco@descher.at>
2024-01-05 16:03:17 +0100

    [25472] error: open guac_exists_result.csv: permission denied

--- docker/ee-util/assets/stage_configure/initializations.sh
+++ docker/ee-util/assets/stage_configure/initializations.sh
@@ -9,8 +9,8 @@
 #
 if [ $ENABLE_GUACAMOLE == true ]; then
     MYSQL_STRING="SELECT EXISTS ( SELECT 1 FROM information_schema.tables WHERE table_schema = 'ee_guacamole' AND LOWER(table_name) = LOWER('guacamole_connection') ) AS TableExists;"
-    timeout 5 /usql --set SHOW_HOST_INFORMATION=false -C mysql://${RDBMS_GUACAMOLE_USERNAME}:${RDBMS_GUACAMOLE_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_GUACAMOLE_DATABASE} -t -c "$MYSQL_STRING" -o guac_exists_result.csv
-    if grep -q "0" guac_exists_result.csv; then
+    timeout 5 /usql --set SHOW_HOST_INFORMATION=false -C mysql://${RDBMS_GUACAMOLE_USERNAME}:${RDBMS_GUACAMOLE_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_GUACAMOLE_DATABASE} -t -c "$MYSQL_STRING" -o /tmp/guac_exists_result.csv
+    if grep -q "0" /tmp/guac_exists_result.csv; then
         echo "======== (Guacamole) Creating database"
         timeout 10 /usql mysql://${RDBMS_GUACAMOLE_USERNAME}:${RDBMS_GUACAMOLE_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_GUACAMOLE_DATABASE} -f /stage_configure/guacamole-init-mysql-db.sql
         echo "======== (Guacamole) Creating database done"
@@ -20,5 +20,5 @@ if [ $ENABLE_GUACAMOLE == true ]; then
         timeout 10 /usql --set SHOW_HOST_INFORMATION=false -C mysql://${RDBMS_GUACAMOLE_USERNAME}:${RDBMS_GUACAMOLE_PASSWORD}@${RDBMS_HOST}:${RDBMS_PORT}/${RDBMS_GUACAMOLE_DATABASE} -t -c "$MYSQL_STRING"
         echo "======== (Guacamole) Updating guacadmin password done" 
     fi
-    rm guac_exists_result.csv
+    rm /tmp/guac_exists_result.csv
 fi
