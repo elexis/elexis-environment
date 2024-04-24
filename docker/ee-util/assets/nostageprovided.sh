ea413999ca25f10a6e0333e264ad7cf26172b66b
Marco Descher <marco@descher.at>
2024-04-24 15:28:07 +0200

    [24210] Remove unused system runtest

--- docker/ee-util/assets/nostageprovided.sh
+++ docker/ee-util/assets/nostageprovided.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "No stage provided, please select on of the following cmd"
 echo "[configure.sh]  Pre-Elexis-Env configuration"
-echo "[runtest.sh]    Post-Elexis-Env startup test"
 echo "[ee_start_setup.sh] Executed during elexis-env startup to perform various configuration tasks"
 echo "[cmd name] To execute a specific command"
