47f44b673e97ec51aedfb6ae1ba988aa6bfdb46e
Marco Descher <marco@descher.at>
2024-05-15 09:04:58 +0200

    [26274] Replace keycloak initialization with keycloak-config-cli (#23)

---
+++ docker/ee-util/assets/ee-bin/keycloak/config-cli/readme.md
@@ -0,0 +1,2 @@
+Fetch from https://github.com/adorsys/keycloak-config-cli/releases
+must fetch version of keycloak as defined in keycloak.yml
---
+++ docker/ee-util/assets/stage_ee_start_setup/keycloak/keycloak_functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KCADM=/ee-bin/keycloak/kcadm/kcadm.sh
+KC_CONFIG_CLI_JAR=/ee-bin/keycloak/config-cli/keycloak-config-cli-23.0.7.jar
+
+S="[KEYCLOAK]"
+T=$S
+
+function randomClientSecret {
+    openssl rand -base64 33 | tr -- '+/' '-_'
+}
+
+function getUserId() {
+    $KCADM get users -r ElexisEnvironment --format csv --fields id,username --noquotes | grep ,$1$ | cut -d "," -f1
+}
