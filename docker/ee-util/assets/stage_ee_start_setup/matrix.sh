9f8357707a57affb3f0895d21cf638e7c32ae844
Marco Descher <marco@descher.at>
2024-09-18 14:32:55 +0200

    wip

--- docker/ee-util/assets/stage_ee_start_setup/matrix.sh
+++ docker/ee-util/assets/stage_ee_start_setup/matrix.sh
@@ -2,5 +2,13 @@
 T="[MATRIX] "
 echo "$T $(date)"
 
+HOMESERVER_YAML=/site/matrix/synapse/homeserver.yaml
+echo "$T Writing $HOMESERVER_YAML"
+
+NAMES="client"
+if [ $ENABLE_MATRIX_FEDERATION == "true" ]; then
+    NAMES+=", federation"
+fi
+export NAMES
 SYNAPSE_USER_DATABASE_PASSWORD=$(cat /site/matrix/synapse/postgres_password) \
-envsubst < matrix/homeserver.yaml.template > /site/matrix/synapse/homeserver.yaml
+envsubst < matrix/homeserver.yaml.template > $HOMESERVER_YAML
