41af12dac39dde1f540bdb79996a4c4c42535642
Marco Descher <marco@descher.at>
2025-11-03 13:01:32 +0100

    [27481] Integrate myelexis-server to R3

--- docker/ee-util/assets/stage_ee_start_setup/solr.sh
+++ docker/ee-util/assets/stage_ee_start_setup/solr.sh
@@ -24,3 +24,12 @@ curl -s --user SolrAdmin:${ADMIN_PASSWORD} $SOLR_BASEURL/solr/admin/authenticati
 # https://solr.apache.org/guide/8_11/rule-based-authorization-plugin.html#manage-permissions
 echo "$T Assert ELEXIS-SERVER has indexer role ..." 
 curl -s --user SolrAdmin:${ADMIN_PASSWORD} $SOLR_BASEURL/solr/admin/authorization -H 'Content-type:application/json' -d '{"set-user-role": {"basic": {"ELEXIS-SERVER": ["indexer", "user"] }}}'
+
+# Configure ELEXIS-SERVER User 
+# https://solr.apache.org/guide/8_11/basic-authentication-plugin.html#add-a-user-or-edit-a-password
+echo "$T Assert myelexis-server user ..." 
+curl -s --user SolrAdmin:${ADMIN_PASSWORD} $SOLR_BASEURL/solr/admin/authentication -H 'Content-type:application/json' -d '{"set-user": { "basic": {"myelexis-server":"'$X_EE_SOLR_ELEXIS_SERVER_PASSWORD'"}}}'
+
+# https://solr.apache.org/guide/8_11/rule-based-authorization-plugin.html#manage-permissions
+echo "$T Assert myelexis-server has indexer role ..." 
+curl -s --user SolrAdmin:${ADMIN_PASSWORD} $SOLR_BASEURL/solr/admin/authorization -H 'Content-type:application/json' -d '{"set-user-role": {"basic": {"myelexis-server": ["indexer", "user"] }}}'
